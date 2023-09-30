7b5b58b254afb03180512312749fba06764b001a
anu1134 <agupta2091@gmail.com>
2023-09-30 09:59:21 +0530

    menu page

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,30 +1,48 @@
 import { useParams } from "react-router-dom";
 import { restaurant_info } from "../common/restaurant_info";
-import { REST_IMG_URL } from "../common/constants";
+import { REST_IMG_URL, CDN_URL } from "../common/constants";
 
 const RestaurantMenu = () => {
   const params = useParams();
 
   return (
-    <div className="restaurant-details">
-      <div>
-        <h1>Restaurant Id: {params.id}</h1>
-        <img
-          src={`${REST_IMG_URL}${restaurant_info[0].cloudinaryImageId}`}
-          alt=""
-        ></img>
-        <h2>{restaurant_info[0].name}</h2>
-        <h2>{restaurant_info[0].city}</h2>
-      </div>
+    <div className="flex text-center">
       <div className="rest-menu-list">
-        <h1>Menu:</h1>
-        <h3>{restaurant_info[0].menu.items[0].name}</h3>
-        <h3>{restaurant_info[0].menu.items[0].name}</h3>
-        <h3>{restaurant_info[0].menu.items[0].name}</h3>
-        <h3>{restaurant_info[0].menu.items[0].name}</h3>
-        <h3>{restaurant_info[0].menu.items[0].name}</h3>
-      </div>
-    </div>
+        {restaurant_info[0].menu.items.map(item => (
+           <div
+           data-testid="foodItems"
+           key={item.id}
+           className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+         >
+         <div className="w-9/12">
+           <div className="py-2">
+             <span>{item.name}</span>
+             <span>
+                - ₹
+               {restaurant_info[0].menu.items[0].price
+                   ? restaurant_info[0].menu.items[0].name.price / 100
+                   : restaurant_info[0].menu.items[0].name / 100}
+              </span>
+           </div>
+           <p className="text-xs">{restaurant_info[0].menu.items[0].description}</p>
+         </div>
+         <div className="w-3/12 p-4">
+          <div className="absolute">
+           <button
+             className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg"
+           >
+             Add +
+           </button>
+         </div>
+         <img src={CDN_URL + restaurant_info[0].menu.items[0].imageId} className="w-full" />
+         </div>
+       </div>
+          
+
+      ))}
+           </div>
+           </div>
+       
   );
 };
 
